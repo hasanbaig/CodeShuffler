89c32d7e5c57042a4e6569bcffcd74a7a0467ffa
Hasan Baig <hasan.baig@hotmail.com>
2025-07-15 14:14:03 -0400

    Delete lib directory

--- lib/settings.py
+++
@@ -1,6 +0,0 @@
-include_incorrect_instructions = True
-first_same_X_lines_MCQ = 2
-#set the length of choice in functions.py line 129 at (len(choice) == len(correct_answer)). 
-no_of_choices = 5
-image_x_dim = 250
-image_y_dim = 250
