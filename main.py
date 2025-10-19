6aca48ed913237f142823706f6a99816fe7b7371
ShawnSpitzel <ShawnSpitzel@gmail.com>
2025-10-18 21:04:50 -0400

    changes to main

--- main.py
+++ main.py
@@ -6,8 +6,7 @@
 from lib import settings
 from lib import functions
 import sys
-
-#Reading code, extracting lines separately
+# Reading code, extracting lines separately
 if sys.argv[1]:
     file_name = sys.argv[1]
 else:
@@ -15,47 +14,30 @@ else:
     sys.exit(1)
     
 read_code = open('codefiles/'+file_name)
+
+# Extract correct and incorrect lines from the code file
 correct_sol, wrong_inst, wrong_inst_dict = functions.read_original_code(read_code)
+correct_sol_w_incorrect = functions.incorrect_instructions(correct_sol, wrong_inst)
 
+# Display shuffled question
+shuffled_question = functions.shuffle_sol(correct_sol_w_incorrect)
+functions.print_code(shuffled_question, "##### Shuffled Exam Question #####")
 
-message = "##### Correct Solution #####"
-functions.print_code(correct_sol, message)
+# Generating correct answer, partial credit options, and random choices
+correct_answer, remain_lines = functions.gen_correct_answer(correct_sol, shuffled_question)
+partial_option = functions.generate_partials(len(wrong_inst_dict), shuffled_question, wrong_inst_dict, correct_answer)
+random_choices = functions.gen_random_choices_wICinst(correct_answer, settings.no_of_choices, remain_lines)
 
-
-if settings.include_incorrect_instructions:
-    with_incorrect_insts = functions.incorrect_instructions(correct_sol, wrong_inst)
-    shuffled_sol = functions.shuffle_sol(with_incorrect_insts)
-else:    
-    #Shuffle the original code lines and tag them with line numbers
-    shuffled_sol = functions.shuffle_sol(correct_sol)
-
-message = "***** Shuffled Solution *****"
-functions.print_code(shuffled_sol, message)
-
-#generating the correct answer with the correct order of code lines
-correct_answer, remain_lines = functions.gen_correct_answer(correct_sol, shuffled_sol)
-
-
-
-if settings.include_incorrect_instructions:
-    random_choices = functions.gen_random_choices_wICinst(correct_answer, settings.no_of_choices, remain_lines)
-else:
-    random_choices = functions.gen_random_choices(correct_answer, settings.no_of_choices)
-
-print()
-print("Multiple answers: ", random_choices) 
-print("Correct answer: ", correct_answer)
-print()    
-
-######## Shuffled Code for Image without indendation
+# Generating shuffled code image without indentation
 image_shuffled_sol = []
-for el in range(len(shuffled_sol)):
-    line = shuffled_sol[el].split(")", 1)
+for el in range(len(shuffled_question)):
+    line = shuffled_question[el].split(")", 1)
     image_shuffled_sol.append(line[0] + ") " + line[-1].strip())
-    # print(test_shuffled_sol[el])
-    
-
-#generating shuffled code image
 print("******* Generating Shuffled code image ......")
 functions.convert_to_image(image_shuffled_sol, file_name)
 print(f"Image generated and stored in ~/shuffledcodeimages/ directory as {file_name}.png")
+print()
+print("Correct answer: ", correct_answer)
+print("Multiple answers: ", random_choices) 
+print("Partial credit options: ", partial_option)
+print()
